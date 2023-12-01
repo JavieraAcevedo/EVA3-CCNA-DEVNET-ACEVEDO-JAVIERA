59f06d98332d7bd5d2d703f9d0a8d604b761cc52
JavieraAcevedo <javiera.acevedo.cortes@gmail.com>
2023-12-01 13:54:58 +0000

    Item 2 prueba

---
+++ Calculo-ACEVEDO.py
@@ -0,0 +1,13 @@
+import math
+
+PI = 3.141592
+VALOR1 = (2 * PI) * 3
+VALOR2 = (2 * PI) * 8
+VALOR3 = (2 * PI) * 10
+
+
+print("Para un radio de 3, su circunferienca sera de:", VALOR1 )
+print("Para un radio de 8, su circunferienca sera de:", VALOR2 )
+print("Para un radio de 10, su circunferienca sera de:", VALOR3 )
+
+
